1233a729219e619d487afa8f5b81b86a28fba5ba
danielkasperczyk <danielkasperczyk@gmail.com>
2021-06-23 17:56:41 +0200

    project init

---
+++ src/plugins/apollo.ts
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import VueApollo from 'vue-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const HttpLink = createHttpLink({
+  uri: 'https://www.kiratest03.food-eshop.de/graphql',
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: HttpLink,
+  cache,
+});
+
+Vue.use(VueApollo);
+
+export default new VueApollo({
+  defaultClient: client,
+});
